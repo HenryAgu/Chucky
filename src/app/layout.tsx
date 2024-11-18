f088f95d9ef0d405101da0c32930ef21f39da1c7
Henry Agu <aguhenrychuks@gmail.com>
2024-11-18 21:23:07 +0100

    feat: spotted the issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Crete_Round, Inter, Noto_Sans } from "next/font/google";
 import "./globals.css";
-import { Provider } from "react-redux";
 
 const creteRound = Crete_Round({ subsets: ["latin"], weight: ["400"] });
 const inter = Inter({
