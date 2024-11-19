09b2d67ac351c58919e31114e2ed0eb1cc1f19da
Henry Agu <aguhenrychuks@gmail.com>
2024-11-19 15:19:15 +0100

    feat: delete item from todo

--- src/components/todo-list/TodoBody.tsx
+++ src/components/todo-list/TodoBody.tsx
@@ -12,21 +12,24 @@ interface TodoBodyProps {
   setTodo: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoBody: React.FC<TodoBodyProps> = ({ todo }) => {
+const TodoBody: React.FC<TodoBodyProps> = ({ todo, setTodo }) => {
+  const handleDelete = (id: string) => {
+    setTodo((prev) => prev.filter((item) => item.id !== id)); // Remove the todo by ID
+  };
   return (
     <div className="mt-5 rounded-lg">
       <div className="flex flex-col gap-y-2">
-        {todo.map((todo) => (
+        {todo.map((item) => (
           <div
             className="border-2  p-3 rounded-lg flex items-center justify-between"
-            key={todo.id}
+            key={item.id}
           >
-            <p className="text-sm font-semibold text-[#2D2D2D]">{todo.title}</p>
+            <p className="text-sm font-semibold text-[#2D2D2D]">{item.title}</p>
             <div className="flex items-center gap-x-3.5">
               <button>
                 <FiEdit2 className="text-base text-[#2D2D2D]" />
               </button>
-              <button>
+              <button onClick={() => handleDelete(item.id)}>
                 <FiTrash2 className="text-base text-[#2D2D2D]" />
               </button>
             </div>
